7c22f074269d15cb570ec4388450e02b9ed7a124
Pawelok35 <131856632+Pawelok35@users.noreply.github.com>
2024-09-14 20:06:19 +0200

    const articleTags = article.getAttribute('data-tags');

--- js/script.js
+++ js/script.js
@@ -93,7 +93,7 @@ function generateTags() {
     let tagHtml = '';
 
     /* get tags from data-tags attribute */
-    const articleTags = article.querySelector('data-tags');
+    const articleTags = article.getAttribute('data-tags');
 
     /* split tags into array */
     const articleTagsArray = articleTags.split(' ');
